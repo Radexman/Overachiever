a5663b91235c590518293721266a5fb7b0b8ddf5
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-10-25 15:07:14 +0200

    Navbar Components Separation & Global State

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect, ChangeEvent } from "react";
 import Navbar from "./components/layout/Navbar/Navbar";
 import Home from "./components/Pages/Home/Home";
 import Tasks from "./components/Pages/Tasks/Tasks";
@@ -6,10 +7,28 @@ import Stats from "./components/Pages/Stats/Stats";
 import About from "./components/Pages/About/About";
 
 const App = () => {
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme")! : "emerald",
+  );
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    const localTheme = localStorage.getItem("theme");
+    document.querySelector("html")?.setAttribute("data-theme", localTheme!);
+  }, [theme]);
+
+  const toggleTheme = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      setTheme("night");
+    } else {
+      setTheme("emerald");
+    }
+  };
+
   return (
     <Router>
       <>
-        <Navbar />
+        <Navbar toggleTheme={toggleTheme} />
         <main className="container mx-auto">
           <Routes>
             <Route path="/" element={<Home />} />
---
+++ src/components/layout/Navbar Features/Tracker.tsx
@@ -0,0 +1,19 @@
+import { FaCalendarCheck } from "react-icons/fa";
+
+const Tracker = () => {
+  return (
+    <div className="dropdown dropdown-hover indicator">
+      <span className="badge indicator-item badge-secondary h-6 w-6 rounded-full  bg-primary font-bold">
+        3
+      </span>
+      <div className="grid h-12 w-12 place-items-center rounded-full bg-primary">
+        <FaCalendarCheck size={30} />
+      </div>
+      <div className="menu dropdown-content rounded-box z-[1] mr-6 mt-12 w-32 bg-base-100 p-2 font-bold text-primary-focus shadow-sm shadow-primary-focus">
+        You have 3 tasks
+      </div>
+    </div>
+  );
+};
+
+export default Tracker;
