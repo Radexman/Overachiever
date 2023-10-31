9022ce8c42991797ed90bc84f8d6f34894c88774
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-10-31 14:54:32 +0100

    Button Group Styling

--- src/components/layout/Modal/Modal.tsx
+++ src/components/layout/Modal/Modal.tsx
@@ -10,26 +10,36 @@ const Modal = ({ id }: ModalProps) => {
   const { removeTask } = useContext(AppContext);
 
   return (
-    <div className="pr-2">
+    <div>
       <button
-        className=""
         onClick={() =>
           (
             document.getElementById("my_modal_5") as HTMLDialogElement | null
           )?.showModal()
         }
       >
-        <MdDeleteForever size={25} />
+        <div className="flex items-center">
+          <p className="uppercase">Delete</p>
+          <MdDeleteForever size={25} />
+        </div>
       </button>
-      <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+      <dialog
+        id="my_modal_5"
+        className="modal modal-bottom text-left sm:modal-middle"
+      >
         <div className="modal-box">
-          <h3 className="text-lg font-bold">Are you sure?</h3>
-          <p className="py-4">
+          <h3 className="text-lg font-bold text-primary-focus">
+            Are you sure?
+          </h3>
+          <p className="text-md py-4 font-normal normal-case text-primary-focus">
             This action will remove task and not mark is as completed.
           </p>
           <div className="modal-action">
             <form method="dialog">
-              <button className="btn" onClick={() => removeTask(id)}>
+              <button
+                className="btn btn-primary"
+                onClick={() => removeTask(id)}
+              >
                 Delete Task
               </button>
               <button className="btn">Close</button>
--- src/components/layout/SingleTask/SingleTask.tsx
+++ src/components/layout/SingleTask/SingleTask.tsx
@@ -11,19 +11,27 @@ const SingleTask = ({ task }: SingleTaskProps) => {
   const { todo, important, id, details } = task;
   return (
     <div
-      className={`collapse collapse-arrow rounded-md text-left ${
+      className={`collapse collapse-arrow rounded-md p-0 text-left ${
         important ? "bg-primary text-primary-content" : "bg-secondary-content"
       }`}
     >
       <input type="radio" name="my-accordion-2" />
       <div className="collapse-title text-xl font-medium">{todo}</div>
       <div className="collapse-content">
-        <div className="flex flex-col">
+        <div className="flex flex-col space-y-4">
           {details && <div>{details}</div>}
-          <div className="start flex justify-end">
-            <IoIosCheckmarkCircleOutline size={25} />
-            <BiMessageAltEdit size={25} />
-            <Modal id={id} />
+          <div className="btn-group items-center">
+            <button className="btn btn-md">
+              Complete
+              <IoIosCheckmarkCircleOutline size={25} />
+            </button>
+            <button className="btn btn-md">
+              Update
+              <BiMessageAltEdit size={25} />
+            </button>
+            <button className="btn btn-md">
+              <Modal id={id} />
+            </button>
           </div>
         </div>
       </div>
