34e4d05b7441ad89ded05924534fc0898337eaa8
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-11-04 18:43:30 +0100

    Chart Calculations

--- src/components/Pages/Stats/Stats.tsx
+++ src/components/Pages/Stats/Stats.tsx
@@ -12,27 +12,51 @@ const Stats = () => {
   const tasksRemaining = tasksTotal - completedTasks;
   const completedPercentage = (completedTasks / tasksTotal) * 100;
 
-  // Important Tasks Calculations
+  // Regular tasks
+  let regularTotalTasks = 0;
+  let regularTasksCompleted = 0;
+
+  // Important Tasks Not Completed Amount
   let importantTotalTasks = 0;
   for (const task of taskList) {
     if (task.important) {
       importantTotalTasks++;
+    } else {
+      regularTotalTasks++;
     }
   }
 
-  // Important Tasks Completed
+  // Important Tasks Completed Amount
   let importantTasksCompleted = 0;
   for (const task of completed) {
     if (task.important) {
       importantTasksCompleted++;
+    } else {
+      regularTasksCompleted++;
     }
   }
+
+  // Total Regular Tasks (Not Completed & Completed)
+  const totalRegularTasks = regularTotalTasks + regularTasksCompleted;
+  const totalRemainingRegularTasks = totalRegularTasks - regularTasksCompleted;
+
+  // Regular Tasks Percentage
+  const regularTasksPercentage =
+    (regularTasksCompleted / totalRegularTasks) * 100;
+
+  // Total Important Tasks (Not Completed & Completed)
+  const totalImportantTasks = importantTotalTasks + importantTasksCompleted;
+  const totalRemainingImportantTasks =
+    totalImportantTasks - importantTasksCompleted;
+
+  // Important Tasks Percentage
+  const importantTasksPercentage =
+    (importantTasksCompleted / totalImportantTasks) * 100;
 
   return (
     <div className="container mx-auto px-4 py-10">
       {/* Profile Section */}
       <Profile />
-      {`Important tasks ${importantTotalTasks}, Important tasks completed ${importantTasksCompleted}`}
       <div className="flex items-center justify-center">
         {/* Statistics Section */}
         <div className="stats stats-vertical w-full bg-secondary-content shadow-md shadow-primary-focus lg:stats-horizontal lg:w-auto">
@@ -40,33 +64,37 @@ const Stats = () => {
           <div className="stat">
             <div className="stat-figure text-primary">
               <RadialChart
-                percentage={completedPercentage}
+                percentage={importantTasksPercentage}
                 textColor="text-secondary-focus"
               />
             </div>
             <div className="stat-value">
               {`${
-                isNaN(completedPercentage) ? "0" : completedPercentage.toFixed()
+                isNaN(importantTasksPercentage)
+                  ? "0"
+                  : importantTasksPercentage.toFixed()
               }%`}
             </div>
-            <div className="stat-title">All Tasks Completed</div>
-            <div className="stat-desc text-secondary">{`${tasksRemaining} tasks remaining`}</div>
+            <div className="stat-title">Important Tasks Completed</div>
+            <div className="stat-desc text-secondary">{`${totalRemainingImportantTasks} tasks remaining`}</div>
           </div>
           {/* Single Stat Regular */}
           <div className="stat">
             <div className="stat-figure text-primary">
               <RadialChart
-                percentage={completedPercentage}
+                percentage={regularTasksPercentage}
                 textColor="text-secondary-focus"
               />
             </div>
             <div className="stat-value">
               {`${
-                isNaN(completedPercentage) ? "0" : completedPercentage.toFixed()
+                isNaN(regularTasksPercentage)
+                  ? "0"
+                  : regularTasksPercentage.toFixed()
               }%`}
             </div>
-            <div className="stat-title">All Tasks Completed</div>
-            <div className="stat-desc text-secondary">{`${tasksRemaining} tasks remaining`}</div>
+            <div className="stat-title">Regular Tasks Completed</div>
+            <div className="stat-desc text-secondary">{`${totalRemainingRegularTasks} tasks remaining`}</div>
           </div>
           {/* Single Stat All */}
 
