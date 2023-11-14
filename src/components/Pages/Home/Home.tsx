0595780f42d516e876997a8be5bebd7360bc94f1
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-11-14 19:40:08 +0100

    Hero Styling

--- src/components/Pages/Home/Home.tsx
+++ src/components/Pages/Home/Home.tsx
@@ -1,28 +1,30 @@
 import { Link } from "react-router-dom";
-import image from "../../../assets/illustration-sign-up-desktop.svg";
+import imageTwo from "../../../assets/hero-img.svg";
 
 const Home = () => {
   return (
-    <div className="flex h-[80vh] items-center justify-start px-8">
-      <img
-        src={image}
-        alt="Statistics and graphs"
-        className="hidden h-[60vh] md:block"
-      />
-      <div className="ml-0 max-w-2xl px-4 text-center md:ml-16 md:text-left">
-        <h1 className="mb-5 text-4xl font-bold md:text-5xl xl:text-7xl">
-          Achieve Your Daily Tasks With Ease
-        </h1>
-        <p className="mb-5 text-lg">
-          Create, track and update your daily tasks and chores. Never again miss
-          a meeting or something that you ought to do. View your current
-          progress and get notified about upcoming assignments.
-        </p>
-        <Link to="/tasks">
-          <button className="btn btn-secondary btn-outline btn-wide my-4">
-            Create New Task
-          </button>
-        </Link>
+    <div
+      className="h-[90vh] bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${imageTwo})`,
+      }}
+    >
+      <div className="flex items-center justify-start pt-0 md:pt-36">
+        <div className="itmes-center ml-0 flex h-screen w-full max-w-2xl flex-col justify-center rounded-none bg-secondary-content/80 p-5 text-center md:ml-16 md:h-auto md:rounded-3xl md:bg-secondary-content/90 md:text-left">
+          <h1 className="mb-5 text-5xl font-bold xl:text-7xl">
+            Achieve Your Daily Tasks With Ease
+          </h1>
+          <p className="mb-5 text-xl">
+            Create, track and update your daily tasks and chores. Never again
+            miss a meeting or something that you ought to do. View your current
+            progress and get notified about upcoming assignments.
+          </p>
+          <Link to="/tasks">
+            <button className="btn btn-secondary btn-outline btn-wide my-4">
+              Create New Task
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
