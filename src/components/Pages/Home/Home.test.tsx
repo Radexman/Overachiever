507fed7a0ebc385cfbbc9e6abf87a2fe0e6f0c84
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-11-16 23:10:15 +0100

    Home Test

---
+++ src/components/Pages/Home/Home.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import Home from "./Home";
+
+describe("Home component should", () => {
+  test("render", () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Home />
+      </MemoryRouter>,
+    );
+    const heading = screen.getByRole("heading", {
+      level: 1,
+      name: "Achieve Your Daily Tasks With Ease",
+    });
+    expect(heading).toBeInTheDocument();
+
+    const tasksSectionButton = screen.getByRole("button");
+    expect(tasksSectionButton).toBeInTheDocument();
+
+    userEvent.click(tasksSectionButton);
+    const tasksHeading = screen.getByText("Create New Task");
+    expect(tasksHeading).toBeInTheDocument();
+  });
+});
