1914c2b7a681e94a8f9d03012d7ee446ef5bec40
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-11-16 23:27:19 +0100

    Tests Refactor

--- src/components/Pages/Home/Home.test.tsx
+++ src/components/Pages/Home/Home.test.tsx
@@ -4,22 +4,38 @@ import userEvent from "@testing-library/user-event";
 import Home from "./Home";
 
 describe("Home component should", () => {
-  test("render", () => {
+  test("render with essential elements", () => {
     render(
       <MemoryRouter initialEntries={["/"]}>
         <Home />
       </MemoryRouter>,
     );
+
     const heading = screen.getByRole("heading", {
       level: 1,
       name: "Achieve Your Daily Tasks With Ease",
     });
     expect(heading).toBeInTheDocument();
 
-    const tasksSectionButton = screen.getByRole("button");
+    const tasksSectionButton = screen.getByRole("button", {
+      name: "Create New Task",
+    });
     expect(tasksSectionButton).toBeInTheDocument();
+  });
+
+  test("navigate to tasks page on button click", () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Home />
+      </MemoryRouter>,
+    );
+
+    const tasksSectionButton = screen.getByRole("button", {
+      name: "Create New Task",
+    });
 
     userEvent.click(tasksSectionButton);
+
     const tasksHeading = screen.getByText("Create New Task");
     expect(tasksHeading).toBeInTheDocument();
   });
