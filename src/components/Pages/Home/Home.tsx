7caf2d73385643209ad252671bcc45765093ee52
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-11-22 20:25:12 +0100

    Home Page Update

--- src/components/Pages/Home/Home.tsx
+++ src/components/Pages/Home/Home.tsx
@@ -5,24 +5,24 @@ const Home = () => {
   return (
     <div
       data-testid="home"
-      className="bg-cover bg-center md:min-h-[90vh]"
+      className="min-h-[80vh] bg-cover bg-center md:min-h-[90vh]"
       style={{
         backgroundImage: `url(${imageTwo})`,
       }}
     >
       <div className="flex justify-start pt-0 md:pt-36">
-        <div className="ml-0 h-screen w-full max-w-2xl rounded-none bg-secondary-content/80 px-7 pt-20 text-center md:ml-16 md:h-auto md:rounded-3xl md:bg-secondary-content/90 md:p-5 md:pt-5 md:text-left">
+        <div className="ml-0 min-h-[80vh] w-full max-w-2xl rounded-none bg-secondary-content/80 px-3 pt-8 text-center md:ml-16 md:h-auto md:min-h-min md:rounded-3xl md:bg-secondary-content/90 md:p-5 md:pt-5 md:text-left">
           <h1 className="mb-5 text-5xl font-bold xl:text-7xl">
             Achieve Your Daily Tasks With Ease
           </h1>
           <p className="mb-5 text-xl">
-            Create, track and update your daily tasks and chores. Never again
-            miss a meeting or something that you ought to do. View your current
-            progress and get notified about upcoming assignments
+            Elevate your performance with detailed progress reports. Track your
+            success, customize your tasks, and unleash your potential. Dive into
+            detailed performance insights and redefine your achievement game!
           </p>
           <Link to="/tasks">
             <button className="btn btn-secondary btn-outline btn-wide my-4">
-              Create New Task
+              Start Overachieving
             </button>
           </Link>
         </div>
